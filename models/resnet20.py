986d21a90f6edad023c4d927adef8756eed64599
Alexia Jolicoeur-Martineau <alexia.jolicoeur-martineau@mail.mcgill.ca>
2023-09-26 12:02:47 -0400

    Update resnet20.py

--- models/resnet20.py
+++ models/resnet20.py
@@ -56,7 +56,7 @@ class BasicBlock(nn.Module):
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
+        out = out + self.shortcut(x)
         out = F.relu(out)
         return out
 
